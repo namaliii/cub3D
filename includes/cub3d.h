f71c5c32e1fb93e6a387f6e6f37e3d2fd3a9dbd2
Alicja <alicjanamieta@gmail.com>
2024-08-10 16:10:12 +0200

    mlx init

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
-/*                                                                            */
+/*		                                                                    */
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
+/*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 21:22:52 by tunsal            #+#    #+#             */
-/*   Updated: 2024/08/07 21:54:57 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/08/10 16:01:32 by anamieta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,25 @@
 # define CUB3D_H
 
 # include <stdio.h>
+# include <fcntl.h>
 # include "get_next_line.h"
+# include "lib/libft/libft.h"
+# include "lib/MLX42/include/MLX42/MLX42.h"
+
+# define WIN_WIDTH 640
+# define WIN_HEIGHT 480
 
 void	parser(int argc, char *argv[]);
+
+typedef struct s_point
+{
+	int x;
+	int y;
+}			   t_point;
+
+typedef struct s_data
+{
+	mlx_t   *mlx;
+}			   t_data;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
+/*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 21:22:28 by tunsal            #+#    #+#             */
-/*   Updated: 2024/08/08 18:09:18 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/08/10 16:09:37 by anamieta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 
 int	main(int argc, char *argv[])
 {
+	static t_data	data;
+
 	printf("this is main\n");
 	parser(argc, argv);
+	data.mlx = mlx_init(WIN_WIDTH, WIN_HEIGHT, "CUB3D", false);
+	if (!data.mlx)
+		return (1);
 }
