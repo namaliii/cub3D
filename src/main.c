5fe2d6f527e6b2d6c0bc15cdbd2346d84e1b8e96
upperdim <45048352+upperdim@users.noreply.github.com>
2024-08-12 17:20:08 +0200

    init screen

--- src/main.c
+++ src/main.c
@@ -6,16 +6,34 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 21:22:28 by tunsal            #+#    #+#             */
-/*   Updated: 2024/08/12 16:07:43 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/08/12 17:07:32 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+static void	init(t_screen *screen)
+{
+	screen->width = SCREEN_WIDTH;
+	screen->height = SCREEN_HEIGHT;
+	screen->window = mlx_init(screen->width, screen->height, "cub3d", false);
+	if (screen->window == NULL)
+		exit_error(mlx_strerror(mlx_errno)); // TODO: mlx_close_window()
+	screen->img = mlx_new_image(screen->window, screen->width, screen->height);
+	if (screen->img == NULL)
+		exit_error(mlx_strerror(mlx_errno)); // TODO: mlx_close_window()
+	if (mlx_image_to_window(screen->window, screen->img, 0, 0) == -1)
+		exit_error(mlx_strerror(mlx_errno)); // TODO: mlx_close_window()
+}
+
 int	main(int argc, char *argv[])
 {
+	t_screen	screen;
+	t_player	player;
 	t_map		*map;
 
 	map = parse(argc, argv);
-	
+	init_player(&player, map);
+	init(&screen);
+	raycast(&screen, map, &player);
 }
