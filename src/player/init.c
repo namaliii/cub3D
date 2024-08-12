4c1323f29bd4facf7078dd2d7f8edfdeb7c10b60
upperdim <45048352+upperdim@users.noreply.github.com>
2024-08-12 18:29:25 +0200

    fix player initialization

--- src/player/init.c
+++ src/player/init.c
@@ -6,35 +6,47 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 15:04:17 by tunsal            #+#    #+#             */
-/*   Updated: 2024/08/12 17:17:47 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/08/12 18:26:08 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static t_vec2d	player_get_spawn_coords(t_map *map)
-{
-	return ((t_vec2d) {0.0, 0.0});
-}
+// static t_vec2d	player_get_spawn_coords(t_map *map)
+// {
+// 	return ((t_vec2d) {0.0, 0.0});
+// }
 
-static float	player_get_spawn_angle(t_map *map)
-{
-	return (0.0);
-}
+// static float	player_get_spawn_angle(t_map *map)
+// {
+// 	return (0.0);
+// }
 
 void	init_player(t_player *player, t_map *map)
 {
 	int first_free_x, first_free_y;
+	int found_flag = false;
 
-	for (first_free_y = 0; first_free_y < MOCK_MAP_WIDTH; ++first_free_y) {
-		for (first_free_x = 0; first_free_x < MOCK_MAP_HEIGHT; ++first_free_x) {
-			if (map->map[first_free_y, first_free_x] == '0')
-				goto found;	
+	for (first_free_y = 0; first_free_y < MOCK_MAP_WIDTH; ++first_free_y)
+	{
+		for (first_free_x = 0; first_free_x < MOCK_MAP_HEIGHT; ++first_free_x)
+		{
+			if (map->map[first_free_y][first_free_x] == '0')
+			{
+				player->posx = first_free_x;
+				player->posy = first_free_y;
+				found_flag = true;
+				break;
+			}
 		}
+
+		if (found_flag)
+			break;
 	}
 
-found:
-	player->posx = first_free_x;
-	player->posy = first_free_y;
+	if (!found_flag)
+		exit_error("Couldn't find a valid player spawn position.");
+
 	player->angle = 0.0;
+	player->game_over = false;
 }
