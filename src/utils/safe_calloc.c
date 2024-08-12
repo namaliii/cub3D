c2bb802758f931ddca28b3e9a3be2fec47a4cfb2
upperdim <45048352+upperdim@users.noreply.github.com>
2024-08-12 16:08:02 +0200

    return the allocation

--- src/utils/safe_calloc.c
+++ src/utils/safe_calloc.c
@@ -6,7 +6,7 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 15:27:22 by tunsal            #+#    #+#             */
-/*   Updated: 2024/08/12 15:29:32 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/08/12 16:06:24 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,5 @@ void	*safe_calloc(size_t elems_count, size_t elem_size)
 	result = ft_calloc(elems_count, elem_size);
 	if (result == NULL)
 		exit_error();
+	return (result);
 }
