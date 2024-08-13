0f95850119c0f567cdf0fee9cb7e12194d2a57fd
upperdim <45048352+upperdim@users.noreply.github.com>
2024-08-13 20:12:18 +0200

    added a debug print for map

--- src/utils/debug_print.c
+++ src/utils/debug_print.c
@@ -6,7 +6,7 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 17:34:55 by tunsal            #+#    #+#             */
-/*   Updated: 2024/08/13 17:46:39 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/08/13 19:44:37 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,25 @@
 static void	print_player(t_game *game)
 {
 	printf("px = %f py = %f pa = %f\n", game->px, game->py, game->p_angle);
+}
+
+void	print_map(t_game *game)
+{
+	int	y;
+	int	x;
+
+	y = 0;
+	while (y < game->map_height)
+	{
+		x = 0;
+		while (x < game->map_width)
+		{
+			printf("%c", game->map[y][x]);
+			++x;
+		}
+		printf("\n");
+		++y;
+	}
 }
 
 void	debug_print(t_game *game)
