e9500e9de54a55bc2664995a9f5a0093add9883d
upperdim <45048352+upperdim@users.noreply.github.com>
2024-08-14 19:15:39 +0200

    prevent drawing outside of minimap

--- src/graphics/minimap.c
+++ src/graphics/minimap.c
@@ -6,18 +6,28 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 18:44:59 by tunsal            #+#    #+#             */
-/*   Updated: 2024/08/14 19:04:59 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/08/14 19:14:15 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static void	draw_line_from_player(t_game *game, float scale_horiz, float scale_vert, float angle_offset)
+static void	draw_line_from_player(t_game *game, float scale_horiz, float scale_vert, float angle_offset, float length, int thickness, t_rgba color)
 {
-	for (float front_dist = 1; front_dist > 0; front_dist -= .05) {
+	for (float front_dist = length; front_dist > 0; front_dist -= .05) {
 		float front_x = game->px + front_dist * sin(game->p_angle_rad + angle_offset);
 		float front_y = game->py + front_dist * cos(game->p_angle_rad + angle_offset);
-		draw_safe_rect(game, front_x * scale_horiz, front_y * scale_vert, 2, 2, (t_rgba) {220, 50, 0, 255});
+
+		if (front_x >= game->map_width || front_y >= game->map_height)
+			continue ;
+
+		draw_safe_rect(
+			game, 
+			front_x * scale_horiz - thickness / 2, 
+			front_y * scale_vert  - thickness / 2, 
+			thickness, 
+			thickness, 
+			color);
 	}
 }
 
@@ -53,10 +63,15 @@ void	draw_minimap(t_game *game)
 	}
 
 	// Draw player
-	draw_safe_rect(game, game->px * scale_horizontal - 4, game->py * scale_vertical - 4, 10, 10, (t_rgba) {255, 127, 0, 255});
+	if (game->px < game->map_width && game->py < game->map_height)
+		draw_safe_rect(game, game->px * scale_horizontal - 4, game->py * scale_vertical - 4, 10, 10, (t_rgba) {255, 127, 0, 255});
 
 	// Draw player direction
-	draw_line_from_player(game, scale_horizontal, scale_vertical, 0);
+	draw_line_from_player(game, scale_horizontal, scale_vertical, 0, 1, 2, (t_rgba) {225, 50, 50, 255});
+
+	draw_line_from_player(game, scale_horizontal, scale_vertical, -game->fov_rad / 2, 6, 1, (t_rgba) {50, 50, 125, 255});
+	draw_line_from_player(game, scale_horizontal, scale_vertical, +game->fov_rad / 2, 6, 1, (t_rgba) {50, 50, 125, 255});
+
 
 	// Draw player rays
 	// float raycast_angle_step = game->fov / game->scr_width;
