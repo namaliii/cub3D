ba856cbfe10852f8b158bd949ee559ec99520bcc
Alicja <alicjanamieta@gmail.com>
2024-08-14 19:09:35 +0200

    No such process message fixed

--- src/parser/cleaning.c
+++ src/parser/cleaning.c
@@ -6,7 +6,7 @@
 /*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 19:00:42 by anamieta          #+#    #+#             */
-/*   Updated: 2024/08/14 18:39:59 by anamieta         ###   ########.fr       */
+/*   Updated: 2024/08/14 19:06:16 by anamieta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ void	error_handling(t_game *game, char **array, char *str)
 		mlx_terminate(game->window);
 		mlx_close_window(game->window);
 	}
-	perror(str);
+	printf("%s", str);
 	free_2d_array(array);
 	exit(EXIT_FAILURE);
 }
