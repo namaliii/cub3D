420fb6b8e0cb40e4f1f8529c5ce5fb9f27fdd373
upperdim <45048352+upperdim@users.noreply.github.com>
2024-08-14 17:51:18 +0200

    support WASD

--- src/player/controls.c
+++ src/player/controls.c
@@ -6,7 +6,7 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 15:43:24 by tunsal            #+#    #+#             */
-/*   Updated: 2024/08/14 17:47:00 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/08/14 17:51:11 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,20 +24,20 @@ void	handler_keyboard(mlx_key_data_t key, void *param)
 		mlx_terminate(game->window);
 		exit(EXIT_SUCCESS);
 	}
-	else if (key.key == MLX_KEY_RIGHT)
+	else if (key.key == MLX_KEY_RIGHT || key.key == MLX_KEY_D)
 	{
 		game->p_angle_rad -= TURN_ANGLE;
 	}
-	else if (key.key == MLX_KEY_LEFT)
+	else if (key.key == MLX_KEY_LEFT || key.key == MLX_KEY_A)
 	{
 		game->p_angle_rad += TURN_ANGLE;
 	}
-	else if (key.key == MLX_KEY_UP)
+	else if (key.key == MLX_KEY_UP || key.key == MLX_KEY_W)
 	{
 		game->px += sin(game->p_angle_rad) * WALK_SPEED;
 		game->py += cos(game->p_angle_rad) * WALK_SPEED;	
 	}
-	else if (key.key == MLX_KEY_DOWN)
+	else if (key.key == MLX_KEY_DOWN || key.key == MLX_KEY_S)
 	{
 		game->px -= sin(game->p_angle_rad) * WALK_SPEED;
 		game->py -= cos(game->p_angle_rad) * WALK_SPEED;
