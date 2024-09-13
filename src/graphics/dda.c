dfd105bd5401c15bdec64d8675ddc6ffdefab1cf
upperdim <45048352+upperdim@users.noreply.github.com>
2024-09-13 10:06:27 +0200

    norm compliance

---
+++ src/graphics/dda.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   dda.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/13 09:42:26 by tunsal            #+#    #+#             */
+/*   Updated: 2024/09/13 10:06:02 by tunsal           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+static void	init_dda_vars(t_dda_vars *v)
+{
+	v->ray_dir_x = 0;
+	v->ray_dir_y = 0;
+	v->delta_dist_x = 0;
+	v->delta_dist_y = 0;
+	v->step_x = 0;
+	v->step_y = 0;
+	v->map_x = 0;
+	v->map_y = 0;
+	v->side_dist_x = 0;
+	v->side_dist_y = 0;
+	v->hit = 0;
+	v->side = 0;
+	v->perp_wall_dist = 0;
+}
+
+static void	setup_vars(float angle, t_game *game, t_dda_vars *v)
+{
+	init_dda_vars(v);
+	v->ray_dir_x = sin(angle);
+	v->ray_dir_y = cos(angle);
+	v->delta_dist_x = sqrt(1 + pow((v->ray_dir_y / v->ray_dir_x), 2));
+	v->delta_dist_y = sqrt(1 + pow((v->ray_dir_x / v->ray_dir_y), 2));
+	if (v->ray_dir_x > 0)
+		v->step_x = 1;
+	else
+		v->step_x = -1;
+	if (v->ray_dir_y > 0)
+		v->step_y = 1;
+	else
+		v->step_y = -1;
+	v->map_x = (int) game->px;
+	v->map_y = (int) game->py;
+	if (v->step_x == 1)
+		v->side_dist_x = (v->map_x + 1.0 - game->px) * v->delta_dist_x;
+	else if (v->step_x == -1)
+		v->side_dist_x = (game->px - v->map_x) * v->delta_dist_x;
+	if (v->step_y == 1)
+		v->side_dist_y = (v->map_y + 1.0 - game->py) * v->delta_dist_y;
+	else if (v->step_y == -1)
+		v->side_dist_y = (game->py - v->map_y) * v->delta_dist_y;
+}
+
+static void	find_distance(t_game *game, t_dda_vars *v)
+{
+	while (v->hit == 0)
+	{
+		if (v->side_dist_x < v->side_dist_y)
+		{
+			v->side_dist_x += v->delta_dist_x;
+			v->map_x += v->step_x;
+			v->side = HIT_VERTICAL_WALL;
+		}
+		else
+		{
+			v->side_dist_y += v->delta_dist_y;
+			v->map_y += v->step_y;
+			v->side = HIT_HORIZONTAL_WALL;
+		}
+		if (is_wall(game, v->map_x, v->map_y))
+			v->hit = 1;
+	}
+}
+
+void	find_dist(float angle, t_game *game, t_ray_hit *p_ray_hit)
+{
+	t_dda_vars	v;
+
+	setup_vars(angle, game, &v);
+	find_distance(game, &v);
+	if (v.side == 0)
+		v.perp_wall_dist = (v.map_x - game->px + (1 - v.step_x) / 2) / \
+v.ray_dir_x;
+	else if (v.side == 1)
+		v.perp_wall_dist = (v.map_y - game->py + (1 - v.step_y) / 2) / \
+v.ray_dir_y;
+	p_ray_hit->side = v.side;
+	p_ray_hit->dist = v.perp_wall_dist;
+}
--- src/graphics/raycast.c
+++ src/graphics/raycast.c
@@ -6,7 +6,7 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 15:38:28 by tunsal            #+#    #+#             */
-/*   Updated: 2024/09/13 09:36:25 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/09/13 09:59:53 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,56 +19,6 @@ void	init_ray_hit(t_ray_hit *p_ray_hit)
 	p_ray_hit->wall_start_px = 0;
 	p_ray_hit->wall_end_px = 0;
 	p_ray_hit->wall_height = 0;
-}
-
-void	find_dist(float angle, t_game *game, t_ray_hit *p_ray_hit)
-{
-	float ray_dir_x = sin(angle);
-	float ray_dir_y = cos(angle);
-	
-	float delta_dist_x = sqrt(1 + pow((ray_dir_y / ray_dir_x), 2));
-	float delta_dist_y = sqrt(1 + pow((ray_dir_x / ray_dir_y), 2));
-	
-	int step_x = (ray_dir_x > 0 ? 1 : -1);
-	int step_y = (ray_dir_y > 0 ? 1 : -1);
-	
-	int map_x = (int) game->px;
-	int map_y = (int) game->py;
-
-	float side_dist_x = 0;
-	if (step_x == 1)       side_dist_x = (map_x + 1.0 - game->px) * delta_dist_x;
-	else if (step_x == -1) side_dist_x = (game->px - map_x) * delta_dist_x;
-
-	float side_dist_y = 0;
-	if (step_y == 1)       side_dist_y = (map_y + 1.0 - game->py) * delta_dist_y;
-	else if (step_y == -1) side_dist_y = (game->py - map_y) * delta_dist_y;
-
-	int hit  = 0;
-	int side = 0;
-	while (hit == 0)
-	{
-		// Jump to the next map square, X or Y direction
-		if (side_dist_x < side_dist_y) {
-			side_dist_x += delta_dist_x;
-			map_x += step_x;
-			side = 0; // hit a vertical wall
-		} else {
-			side_dist_y += delta_dist_y;
-			map_y += step_y;
-			side = 1; // hit a horizontal wall
-		}
-		
-		if (is_wall(game, map_x, map_y))
-			hit = 1;
-	}
-
-	float perp_wall_dist = 0;
-	if (side == 0)
-		perp_wall_dist = (map_x - game->px + (1 - step_x) / 2) / ray_dir_x;
-	else if (side == 1)
-		perp_wall_dist = (map_y - game->py + (1 - step_y) / 2) / ray_dir_y;
-	p_ray_hit->side = side;
-	p_ray_hit->dist = perp_wall_dist;
 }
 
 static void	raycast_column(int x, t_game *game)
@@ -88,11 +38,9 @@ static void	raycast_column(int x, t_game *game)
 	hit_info.wall_start_px = ceiling_end_px;
 	hit_info.wall_end_px = floor_start_px;
 	hit_info.wall_height = floor_start_px - ceiling_end_px;
-	//                   x, y             , xlen, ylen                             , color
-	draw_safe_rect(game, x, 0             , 1   , ceiling_end_px                   , game->color_ceiling);
-	// draw_safe_rect(game, x, ceiling_end_px, 1   , floor_start_px - ceiling_end_px  , (t_rgba) {200, 200, 200, 255});
+	draw_safe_rect(game, x, 0, 1, ceiling_end_px, game->color_ceiling);
 	draw_textured_wall(game, x, angle_rad, &hit_info);
-	draw_safe_rect(game, x, floor_start_px, 1   , game->scr_height - floor_start_px, game->color_floor);
+	draw_safe_rect(game, x, floor_start_px, 1, game->scr_height - floor_start_px, game->color_floor);
 }
 
 void	raycast(t_game *game)
