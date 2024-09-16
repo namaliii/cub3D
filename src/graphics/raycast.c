5423fcba9aa2e5f24bb6bc6dd9bd50bfabba15c5
upperdim <45048352+upperdim@users.noreply.github.com>
2024-09-16 11:15:49 +0200

    fix fisheye in correct var

--- src/graphics/raycast.c
+++ src/graphics/raycast.c
@@ -32,16 +32,14 @@ static void	raycast_column(int x, t_game *game)
 {
 	t_ray_hit	hit_info;
 	float		angle_rad;
-	float		dist;
 	int			ceiling_end_px;
 	int			floor_start_px;
 
 	init_ray_hit(&hit_info);
 	angle_rad = (game->p_angle_rad + game->fov_rad / 2) - ((double) x / (double) game->scr_width) * game->fov_rad;
 	find_dist(angle_rad, game, &hit_info);
-	// TODO: We are correcting fisheye for only this calculation, we don't update the actual damn thing that is used in other places
-	dist = hit_info.dist * cos(angle_rad - game->p_angle_rad);
-	ceiling_end_px = (game->scr_height / 2.0) - (double) game->scr_height / dist;
+	hit_info.dist = hit_info.dist * cos(angle_rad - game->p_angle_rad);
+	ceiling_end_px = (game->scr_height / 2.0) - (double) game->scr_height / hit_info.dist;
 	floor_start_px = game->scr_height - ceiling_end_px;
 	// TODO: Struct t_rect: x, y, xlen, ylen, color?
 	draw_safe_rect(game, x, 0, 1, ceiling_end_px, game->color_ceiling);
