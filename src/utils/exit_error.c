87dc75c2f6079fea1e245c23e733c3f1447e8b6b
upperdim <45048352+upperdim@users.noreply.github.com>
2024-09-16 23:46:56 +0200

    hope this is the right calling structure

--- src/utils/exit_error.c
+++ src/utils/exit_error.c
@@ -3,14 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   exit_error.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 15:20:17 by tunsal            #+#    #+#             */
-/*   Updated: 2024/09/16 18:03:40 by anamieta         ###   ########.fr       */
+/*   Updated: 2024/09/16 23:46:31 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+void	exit_error(const char *msg)
+{
+	if (msg != NULL)
+		printf("%s\n", msg);
+	exit(EXIT_FAILURE);
+}
 
 void	exit_error_parser(t_game *game, char **map, const char *msg)
 {
@@ -30,12 +37,5 @@ void	exit_error_mlx(t_game *game, const char *msg)
 		mlx_close_window(game->window);
 		mlx_terminate(game->window);
 	}
-	exit_error(msg);
-}
-
-void	exit_error(const char *msg)
-{
-	if (msg != NULL)
-		printf("%s\n", msg);
-	exit(EXIT_FAILURE);
+	exit_error_parser(game, game->map, msg);
 }
